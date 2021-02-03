e42b0db07b0a177ce97db8d4e4a184a6d0a2d817
scavanger <andi_kanzler@gmx.de>
2021-02-03 19:50:28 +0100

    DJI OSD improvements
    - Adjustments, using messages via craft name
    - GPS, 3D or Airspeed for GPS field and messages field

--- src/main/io/osd_dji_hd.h
+++ src/main/io/osd_dji_hd.h
@@ -72,10 +72,18 @@ enum djiOsdProtoWorkarounds_e {
     DJI_OSD_USE_NON_STANDARD_MSP_ESC_SENSOR_DATA    = 1 << 0,
 };
 
+enum djiOsdSpeedSouce_e {
+    DJI_OSD_GPS_SPEED   = 0,
+    DJI_OSD_3D_SPEED    = 1,
+    DJI_OSD_AIR_SPEED   = 2,
+};
+
 typedef struct djiOsdConfig_s {
     uint8_t use_name_for_messages;
     uint8_t esc_temperature_source;
     uint8_t proto_workarounds;
+    uint8_t gps_speed_source;
+    uint8_t messages_speed_source;
 } djiOsdConfig_t;
 
 PG_DECLARE(djiOsdConfig_t, djiOsdConfig);
